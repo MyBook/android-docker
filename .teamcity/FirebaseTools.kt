3fa9577a8ab59729300892bdbf207cdf6938aeeb
Pavel Savinov <p.savinov@mybook.ru>
2020-06-01 00:08:27 +0300

    Add VCS params to build type stubs
    #AN-3331

--- .teamcity/FirebaseTools.kt
+++ .teamcity/FirebaseTools.kt
@@ -1,7 +1,31 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.swabra
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 object FirebaseTools : BuildType({
 
     name = "Firebase Tools"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    triggers {
+        vcs {
+            branchFilter = ""
+        }
+    }
+
+    failureConditions {
+        executionTimeoutMin = 5
+    }
+
+    features {
+        swabra {
+            forceCleanCheckout = true
+            verbose = true
+        }
+    }
 
 })
--- .teamcity/JavaJdk.kt
+++ .teamcity/JavaJdk.kt
@@ -1,7 +1,31 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.swabra
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 object JavaJdk : BuildType({
 
     name = "Java Development Kit"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    triggers {
+        vcs {
+            branchFilter = ""
+        }
+    }
+
+    failureConditions {
+        executionTimeoutMin = 5
+    }
+
+    features {
+        swabra {
+            forceCleanCheckout = true
+            verbose = true
+        }
+    }
 
 })

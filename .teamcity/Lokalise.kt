02a41d8fe6977640364d49514bdcff8587aa9b04
Pavel Savinov <p.savinov@mybook.ru>
2020-06-10 20:15:20 +0300

    Add dependencies between build types
    #AN-3331
    
    # Conflicts:
    #	.teamcity/JavaJdk.kt

--- .teamcity/Lokalise.kt
+++ .teamcity/Lokalise.kt
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
+import jetbrains.buildServer.configs.kotlin.v2019_2.FailureAction
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.swabra
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
@@ -7,6 +8,12 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 object Lokalise : BuildType({
 
     name = "Lokalise"
+
+    dependencies {
+        snapshot(Base) {
+            onDependencyFailure = FailureAction.FAIL_TO_START
+        }
+    }
 
     vcs {
         root(DslContext.settingsRoot)
